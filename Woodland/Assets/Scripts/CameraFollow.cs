fb7016aba423b0c597939e0d7ae1f86ad105682b
ijoh785 <ijoh785@aucklanduni.ac.nz>
2021-06-25 21:34:37 +1200

    Add following camera

---
+++ Woodland/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform Player;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+    }
+}
