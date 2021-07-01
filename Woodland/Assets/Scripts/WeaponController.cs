0a908ce00cf0e18f6d4b98973402aa0f37e3f088
ijoh785 <ijoh785@aucklanduni.ac.nz>
2021-07-01 14:42:13 +1200

    add damage trigger for enemies

--- Woodland/Assets/Scripts/WeaponController.cs
+++ Woodland/Assets/Scripts/WeaponController.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnTriggerEnter2D(Collider2D col){
+        if (col.gameObject.tag == "Enemy"){
+            Debug.Log("Enemy hit");
+        }
     }
 }
